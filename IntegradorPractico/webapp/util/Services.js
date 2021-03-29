571d4d40aeec4f55a3b3b715c1b4cca8b8ffd93f
LucasCarino <lucascarino2@gmail.com>
2021-03-29 20:47:36 +0000

    evaluacion terminada

--- IntegradorPractico/webapp/util/Services.js
+++ IntegradorPractico/webapp/util/Services.js
@@ -1,79 +1,79 @@
 sap.ui.define([
     "IntegradorPractico/IntegradorPractico/util/Constants"
 ],
-function (Constants) {
-    "use strict"; return { 
-        getProducts: function () {
-            let oRequest = this.getRequest({
-                endPoint: Constants.REQUEST.endPoint.northwind,
-                method: Constants.REQUEST.method.GET,
-                entity: Constants.REQUEST.entity.products
-            });
-            return this.promisizer(oRequest);
-        },
-        getCategory: function (categoryID) {
-            let oRequest = this.getRequest({
-                endPoint: Constants.REQUEST.endPoint.northwind,
-                method: Constants.REQUEST.method.GET,
-                entity: Constants.REQUEST.entity.products + "(" + categoryID + ")/Category"
-            });
-            return this.promisizer(oRequest);
-        },
-        getSupplier: function (supplierID) {
-            let oRequest = this.getRequest({
-                endPoint: Constants.REQUEST.endPoint.northwind,
-                method: Constants.REQUEST.method.GET,
-                entity: Constants.REQUEST.entity.products + "(" + supplierID + ")/Supplier"
-            });
-            return this.promisizer(oRequest);
-        },
-        promisizer: function (oOptions) {
-            return this.toPromise(jQuery.ajax(oOptions));
-        },
-        toPromise: function (oPromise) {
-            return new Promise(function (resolve, reject) {
-                oPromise.then(() => {
-                    const sHeaders = oPromise.done().getAllResponseHeaders();
-                    const aHeaders = sHeaders.trim().split(/[\r\n]+/);
-                    const oHeaderMap = {};
-                    aHeaders.forEach(function (sLine) {
-                        const aParts = sLine.split(': ');
-                        const sHeader = aParts.shift();
-                        const sValue = aParts.join(': ');
-                        oHeaderMap[sHeader] = sValue;
-                    });
-                    resolve([oPromise.done().responseJSON, oHeaderMap]);
-                }, reject);
-            });
-        },
-        getRequest: function (oOptions = {}) {
-        const oHeaders = {
-        'Content-Type': "application/json;charset=UTF-8;IEEE754Compatible=true",
-        'accept': "application/json",
-        'Access-Control-Allow-Origin': "*"
+    function (Constants) {
+        "use strict"; return {
+            getProducts: function () {
+                let oRequest = this.getRequest({
+                    endPoint: Constants.REQUEST.endPoint.northwind,
+                    method: Constants.REQUEST.method.GET,
+                    entity: Constants.REQUEST.entity.products
+                });
+                return this.promisizer(oRequest);
+            },
+            getCategory: function (categoryID) {
+                let oRequest = this.getRequest({
+                    endPoint: Constants.REQUEST.endPoint.northwind,
+                    method: Constants.REQUEST.method.GET,
+                    entity: Constants.REQUEST.entity.products + "(" + categoryID + ")/Category"
+                });
+                return this.promisizer(oRequest);
+            },
+            getSupplier: function (supplierID) {
+                let oRequest = this.getRequest({
+                    endPoint: Constants.REQUEST.endPoint.northwind,
+                    method: Constants.REQUEST.method.GET,
+                    entity: Constants.REQUEST.entity.products + "(" + supplierID + ")/Supplier"
+                });
+                return this.promisizer(oRequest);
+            },
+            promisizer: function (oOptions) {
+                return this.toPromise(jQuery.ajax(oOptions));
+            },
+            toPromise: function (oPromise) {
+                return new Promise(function (resolve, reject) {
+                    oPromise.then(() => {
+                        const sHeaders = oPromise.done().getAllResponseHeaders();
+                        const aHeaders = sHeaders.trim().split(/[\r\n]+/);
+                        const oHeaderMap = {};
+                        aHeaders.forEach(function (sLine) {
+                            const aParts = sLine.split(': ');
+                            const sHeader = aParts.shift();
+                            const sValue = aParts.join(': ');
+                            oHeaderMap[sHeader] = sValue;
+                        });
+                        resolve([oPromise.done().responseJSON, oHeaderMap]);
+                    }, reject);
+                });
+            },
+            getRequest: function (oOptions = {}) {
+                const oHeaders = {
+                    'Content-Type': "application/json;charset=UTF-8;IEEE754Compatible=true",
+                    'accept': "application/json",
+                    'Access-Control-Allow-Origin': "*"
+                };
+                if (oOptions.username && oOptions.password) {
+                    oHeaders.Authorization = "Basic " + btoa(oOptions.username + ":" + oOptions.password);
+                }
+                if (oOptions.fetch) {
+                    oHeaders['X-CSRF-TOKEN'] = 'Fetch';
+                }
+                if (oOptions.method === 'POST') {
+                    oHeaders['X-CSRF-TOKEN'] = localStorage.getItem('csrf');
+                }
+                const sParams = oOptions.params ? "?" + oOptions.params : "";
+                return {
+                    headers: oHeaders,
+                    url: this.getBaseURL(oOptions.endPoint) + this.getEndpoint(oOptions) + "/" + (oOptions.entity || '') + sParams,
+                    method: oOptions.method,
+                    data: oOptions.data || ''
+                };
+            },
+            getEndpoint: function (oOptions) {
+                return oOptions.endPoint;
+            },
+            getBaseURL: function (sEndPoint) {
+                return jQuery.sap.getModulePath("IntegradorPractico.IntegradorPractico") + "/";
+            }
         };
-        if (oOptions.username && oOptions.password) {
-        oHeaders.Authorization = "Basic " + btoa(oOptions.username + ":" + oOptions.password);
-        }
-        if (oOptions.fetch) {
-        oHeaders['X-CSRF-TOKEN'] = 'Fetch';
-        }
-        if (oOptions.method === 'POST') {
-        oHeaders['X-CSRF-TOKEN'] = localStorage.getItem('csrf');
-        }
-        const sParams = oOptions.params ? "?" + oOptions.params : "";
-        return {
-        headers: oHeaders,
-        url: this.getBaseURL(oOptions.endPoint) + this.getEndpoint(oOptions) + "/" + (oOptions.entity || '') + sParams,
-        method: oOptions.method,
-        data: oOptions.data || ''
-            };
-        },
-        getEndpoint: function (oOptions) {
-        return oOptions.endPoint;
-        },
-        getBaseURL: function (sEndPoint) {
-        return jQuery.sap.getModulePath("IntegradorPractico.IntegradorPractico") + "/";
-        }
-    };
-});
+    });
